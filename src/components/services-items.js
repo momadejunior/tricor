7274016c24764b97ae18e5c58320d1374feb5091
Momade Junior <momadejunior584@gmail.com>
2024-10-29 17:09:10 +0200

    Update services-items.js

--- src/components/services-items.js
+++ src/components/services-items.js
@@ -1,84 +1,84 @@
-export default function ServicesItem(){
-    return(
-        <>
-               <section class="services-area-two pt-120 pb-100">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-xl-7 col-lg-8">
-                        <div class="section-title text-center mb-55 wow fadeInUp delay-0-2s">
-                            <span class="sub-title mb-10">Serviços</span>
-                            <h2>Serviços de Impressão</h2>
-                        </div>
-                    </div>
-                </div>
-                <div class="row justify-content-center">
-                    <div class="col-lg-6">
-                        <div class="service-item-two wow fadeInUp delay-0-2s">
-                            <div class="icon">
-                                <img className="service-items" src="./img/digital.png"/>
-                            </div>
-                            <div class="content">
-                                <h3><a href="service-details.html">Impressão Digital</a></h3>
-                                <p>É ideal para trabalhos de pequenas tiragens e alta personalização.</p>
-                                <ul>
-                               <li>	Cartões de Visita</li>
-                               <li>	Flyers e Panfletos</li>
-                               <li>	Brochuras e Catálogos</li>
-                               <li>	Cartazes e Pôsteres</li>
-                               <li>	Etiquetas e Adesivos</li>
-                                </ul>
-                                <a href="service-details.html" class="read-more">Ler mais <i class="far fa-long-arrow-right"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        <div class="service-item-two wow fadeInUp delay-0-4s">
-                            <div class="icon">
-                            <img className="service-items" src="./img/tshirt-print.png"/>
-                            </div>
-                            <div class="content">
-                                <h3><a href="service-details.html">Impressão Offset</a></h3>
-                                <p>É mais adequada para grandes tiragens e trabalhos de alta qualidade.</p>
-                                <ul>
-                                   <li>Livros e Revistas</li>
-                                   <li>Jornais</li>
-                                   <li>Embalagens Personalizadas</li>
-                                   <li>Papel Timbrado e Envelopes</li>
-                                   <li>Folders e Folhetos</li>
-                                </ul>
-                                <a href="service-details.html" class="read-more">Ler mais <i class="far fa-long-arrow-right"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        <div class="service-item-two wow fadeInUp delay-0-2s">
-                            <div class="icon">
-                                <img className="service-items" src="./img/offset-color.png"/>
-                            </div>
-                            <div class="content">
-                                <h3><a href="service-details.html">Serigrafia para T-Shirts</a></h3>
-                                <p>É versátil para imprimir em materiais diversos e em grandes volumes.</p>
-                                <a href="service-details.html" class="read-more">Ler mais <i class="far fa-long-arrow-right"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        <div class="service-item-two wow fadeInUp delay-0-4s">
-                            <div class="icon">
-                                <img className="service-items" src="./img/sublimacao.png"/>
-                            </div>
-                            <div class="content">
-                                <h3><a href="service-details.html">Sublimação</a></h3>
-                                <p>É ideal para personalização em tecidos e outros materiais revestidos.</p>
-                                <a href="service-details.html" class="read-more">Ler mais <i class="far fa-long-arrow-right"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                   
-                </div>
+import { useEffect, useState } from 'react';
+
+export default function ServicesItem() {
+  const [services, setServices] = useState([]);
+
+  const url = "https://us-west-2.cdn.hygraph.com/content/cm1z3ff5507ct08w75zh4fqp0/master";
+
+  const query = `
+    query ServicosDeImpressao {
+      servicosDeImpressaos {
+        id
+        tituloDoServico
+        tiposDeServicos
+        imagemDoServico {
+          id
+          url
+        }
+      }
+    }
+  `;
+
+  useEffect(() => {
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ query }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setServices(data.data.servicosDeImpressaos); // Store fetched services in state
+      })
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+      });
+  }, []);
+
+  return (
+    <section className="services-area-two pt-120 pb-100">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-xl-7 col-lg-8">
+            <div className="section-title text-center mb-55 wow fadeInUp delay-0-2s">
+              <span className="sub-title mb-10">Serviços</span>
+              <h2>Serviços de Impressão</h2>
             </div>
-        </section>
-      
-        </>
-    )
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          {services.map((service) => (
+            <div className="col-lg-6" key={service.id}>
+              <div className="service-item-two wow fadeInUp delay-0-2s">
+                <div className="icon">
+                  <img 
+                    className="service-items" 
+                    src={service.imagemDoServico?.url || "./img/default-icon.png"} 
+                    alt={service.tituloDoServico} 
+                  />
+                </div>
+                <div className="content">
+                  <h3><a href="service-details.html">{service.tituloDoServico}</a></h3>
+                  <ul>
+                    {service.tiposDeServicos.map((tipo, index) => (
+                      <li key={index}>{tipo}</li>
+                    ))}
+                  </ul>
+                  <a href="service-details.html" className="read-more">
+                    Ler mais <i className="far fa-long-arrow-right"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
